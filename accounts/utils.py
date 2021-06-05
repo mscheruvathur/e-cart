d2296661e8f1d56e063e3035be4343d492f03a07
samtirur <mscheruvathur@gmail.com>
2021-06-05 08:39:14 +0530

    domain edited

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,5 +1,5 @@
 import uuid
 
 def generate_ref_code():
-    code = 'http://127.0.0.1:8000/store/' + str(uuid.uuid4()).replace("-", "")[:12]
+    code = 'http://ec2-18-222-163-8.us-east-2.compute.amazonaws.com/store/' + str(uuid.uuid4()).replace("-", "")[:12]
     return code
